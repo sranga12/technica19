84dfc008749f6054e7de019b4ab814098bab5b78
sranga12 <35714412+sranga12@users.noreply.github.com>
2019-11-14 10:22:08 -0500

    uploaded

--- facedetection.js
+++ facedetection.js
@@ -1,51 +1,25 @@
-'use strict';
-
-const request = require('request');
 const express = require('express')
 const app = express()
-const port = 3000
+var path = require('path');
+var bodyParser = require('body-parser');
+//const maxApi = require('max-api')
 
+var imageData = null;
+app.use(bodyParser.json()); // to support JSON-encoded bodies
 
-// Replace <Subscription Key> with your valid subscription key.
-const subscriptionKey = 'f1294c049ee2412993503ad5aae58252';
+app.use(
+    bodyParser.urlencoded({
+        // to support URL-encoded bodies
+        extended: true
+    })
+);
 
-// You must use the same location in your REST call as you used to get your
-// subscription keys. For example, if you got your subscription keys from
-// westus, replace "westcentralus" in the URL below with "westus".
-const uriBase = 'https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect';
-
-// Request parameters.
-
-app.get('/', (req, res) => res.sendFile("myindex.html"));
+app.get('/', (req, res) => res.sendFile(path.resolve("./myindex.html")));
 
 app.listen(3000, () => console.log(`Example app listening on port 3000!`));
 
-app.post("/sendPicture", (req, res) => {
-
-    const params = {
-        'returnFaceId': 'true',
-        'returnFaceLandmarks': 'false',
-        'returnFaceAttributes': 'age,gender,headPose,smile,facialHair,glasses,' +
-            'emotion,hair,makeup,occlusion,accessories,blur,exposure,noise'
-    };
-
-    const options = {
-        uri: uriBase,
-        qs: params,
-        body: '{"url": ' + '"' +'image/webp'+ '"}',
-        headers: {
-            'Content-Type': 'application/json',
-            'Ocp-Apim-Subscription-Key': subscriptionKey
-        }
-    };
-
-    request.post(options, (error, response, body) => {
-        if (error) {
-            console.log('Error: ', error);
-            return;
-        }
-        let jsonResponse = JSON.stringify(JSON.parse(body), null, '  ');
-        console.log('JSON Response\n');
-        console.log(jsonResponse);
-    });
+app.post("/saveData", (req, res) => {
+    imageData = req.body.data;
+    console.log(imageData[0])
+    res.sendStatus(200)
 });
